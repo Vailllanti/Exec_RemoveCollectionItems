ca0d3bb29186fd4b2a5538da7d99613cef521015
建均 陳 <protosscarrier@gmail.com>
2022-10-22 17:08:50 +0800

    加入專案檔案。

---
+++ 在集合中移除多個項目/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 在集合中移除多個項目
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> list = new List<string>() 
+			{
+				"aa",
+				"bb",
+				"cc",
+				"c",
+				"dd",
+
+			};
+			list.Remove("cc");
+			list.Remove("dd");
+			list.Clear();
+		}
+	}
+}
